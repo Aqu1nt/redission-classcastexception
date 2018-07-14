e173ecce9719b286342b1ba8338e14c0be5b7db2
von Wattenwyl Emil <emil.vonwattenwyl@sitasys.com>
2018-07-14 15:50:16 +0200

    Add gradle application plugin

--- src/main/java/ch/aquint/RedissonClassCastException.java
+++ src/main/java/ch/aquint/RedissonClassCastException.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedissionClasscastException {
+public class RedissonClassCastException {
 
   public static void main(String[] args) {
-    SpringApplication.run(RedissionClasscastException.class);
+    SpringApplication.run(RedissonClassCastException.class);
   }
 }
